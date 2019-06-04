29a43b5bb7f8138416cf9a1d8e07f788b679f740
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-04 21:16:54 +0530

    Version 1 full upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Memory-Card-Game
-Memory Card Game using VanillaJS and CSS3 animations and transitions
+Memory Card Game using VanillaJS and CSS3 animations and transitions.
+
+## Preview
+Click [here](https://somsubhra1.github.io/Memory-Card-Game/) for preview.
--- js/script.js
+++ js/script.js
@@ -1,7 +1,63 @@
+//Grabbing DOM Elements
 const cards = document.querySelectorAll('.memory-card');
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+// Setting globals
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
+// Adding eventListeners to each card
+cards.forEach(card => card.addEventListener('click', flipCard));
+// Adding eventListener to window for shuffling
+window.addEventListener('load', shuffle);
+
+// Flipping card
 function flipCard() {
-    this.classList.toggle('flip'); 
+    if (lockBoard || this === firstCard) {
+        return;
+    }
+    this.classList.toggle('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;        
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;  
+        matchCard(firstCard, secondCard);
+    }    
+}
+
+// function to match card
+function matchCard(firstCard, secondCard) {
+    if (firstCard.dataset.name === secondCard.dataset.name) { // matching dataset attr
+        // removing event listeners if it's a match
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+        firstCard.style.cursor = 'default';
+        secondCard.style.cursor = 'default';
+        reset();
+    }
+    else {
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            // lockBoard = false;
+            reset();
+        }, 1500);        
+    }
+}
+
+// Reset function
+function reset() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+// Shuffle card function
+function shuffle() {
+    cards.forEach(card => {
+        let random = Math.floor(Math.random() * 12);
+        card.style.order = random;
+    })
 }
